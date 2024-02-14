c8d804005b5bca07e2f4d78333d94aa339465def
Dhenyllo Santos <dhenyllosantos@gmail.com>
2024-02-14 17:34:01 -0300

    update no page.tsx

---
+++ src/components/formEmail.tsx
@@ -0,0 +1,13 @@
+
+function FormEmail() {
+    return (
+        <div>
+            <p>Enter your email address</p>
+            <input type="text" />
+            <button>Submit</button>
+        
+        </div>
+    )
+}
+
+export default FormEmail;
