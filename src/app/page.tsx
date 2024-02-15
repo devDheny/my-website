42e62767282f5851bf597e371efdec7d144349f4
Dhenyllo Santos <dhenyllosantos@gmail.com>
2024-02-15 20:33:43 -0300

    nova home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 
 import { LinkedInLogoIcon as LinkedIn, GitHubLogoIcon as GitHub } from "@radix-ui/react-icons";
-
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap')
+</style>
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col md:flex-col items-center justify-center p-24 bg-gradient-to-b from-slate-900 to-slate-800">
-      <h1 className="text-6xl md:text-8xl font-bold font-mono text-center text-slate-100"><span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 animate-pulse">&#123;</span>Denilo<br />Feitosa<span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 animate-pulse">&#125;</span></h1>
-      <p className="text-lg font-bold font-arial text-center text-slate-100 px-4 py-1 mt-2 text-slate-800 rounded bg-gradient-to-r from-pink-500 to-violet-500">Front-end Dev</p>
+    <main id="home" className="flex min-h-screen flex-col md:flex-row items-center justify-center p-24 bg-gradient-to-b from-slate-900 to-slate-800">
+      <h1 className="text-6xl md:text-8xl font-bold text-center text-slate-100">Dheny<span className="text-3xl md:text-4xl font-light font-arial text-center bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 uppercase">&#46;Dev</span></h1>
     </main>
   );
 }
