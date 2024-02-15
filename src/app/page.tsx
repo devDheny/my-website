339b8edf80be62b20f6db285454ae35b4ae5249e
Dhenyllo Santos <dhenyllosantos@gmail.com>
2024-02-15 19:03:40 -0300

    :D

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+
+import { LinkedInLogoIcon as LinkedIn, GitHubLogoIcon as GitHub } from "@radix-ui/react-icons";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold text-center">Dhenyllo<br/>Santos</h1>
+    <main className="flex min-h-screen flex-col md:flex-col items-center justify-center p-24 bg-gradient-to-b from-slate-900 to-slate-800">
+      <h1 className="text-6xl md:text-8xl font-bold font-mono text-center text-slate-100"><span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 animate-pulse">&#123;</span>Denilo<br />Feitosa<span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 animate-pulse">&#125;</span></h1>
+      <p className="text-lg font-bold font-arial text-center text-slate-100 px-4 py-1 mt-2 text-slate-800 rounded bg-gradient-to-r from-pink-500 to-violet-500">Front-end Dev</p>
     </main>
   );
 }
